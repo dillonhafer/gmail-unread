5feba3995bca1778b4cc5c7dfad8d22dc3794081
Dillon Hafer <dh@dillonhafer.com>
2015-06-07 22:20:23 -0400

    Add content script

---
+++ contentscript.js
@@ -0,0 +1,47 @@
+(function() {
+  var intervalID = 0
+
+  function createUnreadButton() {
+    var unreadDiv, buttonDiv, unreadText
+    unreadDiv = document.createElement('div')
+    unreadDiv.className = 'G-Ni J-J5-Ji'
+
+    buttonDiv = document.createElement('div')
+    buttonDiv.className = 'T-I J-J5-Ji ar7 nf T-I-ax7 L3'
+    buttonDiv.setAttribute('role', 'button')
+
+    unreadText = document.createElement('span')
+    unreadText.innerHTML = 'Unread'
+
+    buttonDiv.appendChild(unreadText)
+    unreadDiv.appendChild(buttonDiv)
+    return unreadDiv
+  }
+
+  function buttonsOnPage() {
+    var buttons = document.getElementsByClassName('Ykrj7b')
+    return Boolean(buttons.length)
+  }
+
+  function buttonList() {
+    var buttons = document.getElementsByClassName('Ykrj7b')
+    return buttons[0].parentElement.parentElement.parentElement
+  }
+
+  function showUnread() {
+    alert("Here are your unread emails")
+  }
+
+  function main() {
+    var buttonDiv, unreadButton
+    if (buttonsOnPage()) {
+      buttonDiv    = buttonList()
+      unreadButton = createUnreadButton()
+      unreadButton.addEventListener('click', showUnread)
+      buttonDiv.appendChild(unreadButton)
+      clearInterval(intervalID)
+    }
+  }
+
+  intervalID = setInterval(main, 800)
+})()
